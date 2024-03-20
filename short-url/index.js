ba8b4129a6471c28cebdce426cf6226c02687142
AdnanAShaikh <shaikhadnan862@gmail.com>
2024-03-20 19:55:43 +0530

    started learning ejs

--- short-url/index.js
+++ short-url/index.js
@@ -3,11 +3,14 @@ const urlRoute = require("./routes/url");
 const URL = require("./model/url");
 const { connectToMongoDB } = require("./connection");
 const app = express();
+const path = require("path");
+const ejs = require("ejs");
 
 connectToMongoDB("mongodb://127.0.0.1:27017/short-url").then(() =>
   console.log("Mongo connected ! ! !")
 );
-
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 app.use(express.json());
 
 app.use("/url", urlRoute);
